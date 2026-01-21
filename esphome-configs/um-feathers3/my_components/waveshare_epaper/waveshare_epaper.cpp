f403d34401be9b596c69c56e1edb4b502c3da194
ME-DataStudio <75776348+ME-DataStudio@users.noreply.github.com>
2026-01-21 15:40:55 +0100

    Update waveshare_epaper.cpp

--- esphome-configs/um-feathers3/my_components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome-configs/um-feathers3/my_components/waveshare_epaper/waveshare_epaper.cpp
@@ -4756,13 +4756,16 @@ void WaveshareEPaper13P3InK::dump_config() {
 //  - https://github.com/waveshareteam/e-Paper/tree/master/Arduino/epd4in37g
 // ========================================================
 void WaveshareEPaper4P37In::initialize() {
-  this->command(0xAA);
-  this->data(0x49);
-  this->data(0x55);
-  this->data(0x20);
-	this->data(0x08);
-	this->data(0x09);
-  this->data(0x18);
+//  this->command(0xAA);
+//  this->data(0x49);
+//  this->data(0x55);
+//  this->data(0x20);
+//  this->data(0x08);
+//  this->data(0x09);
+//  this->data(0x18);
+  this->wait_until_idle_();
+  this->command(0x12);  // SWRESET
+  this->wait_until_idle_();	
 
   this->command(0x01);
   this->data(0x3F);
@@ -4799,28 +4802,28 @@ void WaveshareEPaper4P37In::initialize() {
   this->data(0x00);
   this->data(0x44); 
 
-  this->command(0x50);
-  this->data(0x3F);
+ // this->command(0x50);
+ // this->data(0x3F);
 
-  this->command(0x60);
-  this->data(0x02);
-  this->data(0x00);
+ // this->command(0x60);
+ // this->data(0x02);
+ // this->data(0x00);
 
 	//Please notice that PLL must be set for version 2 IC
-  this->command(0x30);
-  this->data(0x08);
+ // this->command(0x30);
+ // this->data(0x08);
 	
-  this->command(0x61);
-  this->data(0x02);
-  this->data(0x00);
-  this->data(0x01); 
-  this->data(0x70); 
+ // this->command(0x61);
+ // this->data(0x02);
+ // this->data(0x00);
+ // this->data(0x01); 
+ // this->data(0x70); 
 
-  this->command(0xE3);
-  this->data(0x2F);
+ // this->command(0xE3);
+ // this->data(0x2F);
 
-  this->command(0x84);
-  this->data(0x01);
+ // this->command(0x84);
+ // this->data(0x01);
 
 }
 void HOT WaveshareEPaper4P37In::display() {
